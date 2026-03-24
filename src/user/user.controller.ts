24d61478b38d55111d032b9345db23bc9d38f150
IkrtI <minipro787@gmail.com>
2026-03-24 15:31:49 +0700

    FF

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,51 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  NotFoundException,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { IUser } from './user.entity';
 
-@Controller()
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get("test")
-  getTest(): string {
-    return this.UserService.getTest();
+  @Get()
+  findAll(): Promise<IUser[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(
+    @Param('id') id: string,
+    @Query('fields') fields: string = '',
+  ): Promise<unknown> {
+    const users = await this.userService.findAll();
+    const user = users.find((U: IUser) => U.id == id);
+    if (!user) {
+      return new NotFoundException({
+        statusCode: 404,
+        message: 'User not found',
+        error: 'Not Found',
+      });
+    }
+    if (fields) {
+      const fieldList = fields.split(',').map((f) => f.trim());
+      const filteredUser: Record<string, unknown> = {};
+      fieldList.forEach((field) => {
+        if (Object.prototype.hasOwnProperty.call(user, field)) {
+          filteredUser[field] = user[field];
+        }
+      });
+      return filteredUser;
+    }
+    return user;
+  }
+
+  @Get('test')
+  getTest(): [] {
+    return this.userService.getTest();
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { IUser } from './user.entity';
+import { readFile } from 'fs/promises';
 
 @Injectable()
-export class AppService {
-  getTest(): string {
-    return [] 
-  } 
-   
-  findAll(): array {}
+export class UserService {
+  getTest(): [] {
+    return [];
+  }
+
+  async findAll(): Promise<IUser[]> {
+    return JSON.parse(await readFile('./data/users.json', 'utf-8')) as IUser[];
+  }
 }
