db85003a6bca1cb5ef3748797c1166f44e2e21c7
Ikrt.dev <57593203+IkrtI@users.noreply.github.com>
2026-03-24 14:54:45 +0700

    F

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Welcome to NestJS!';
-  }
+  getTest(): string {
+    return [] 
+  } 
+   
+  findAll(): array {}
 }
